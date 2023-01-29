77c38b72e903fbc358634362a838b050d392032a
joshjh2002 <joshhelman@hotmail.co.uk>
2023-01-29 15:18:15 +0000

    Moved Rust Embed to embeds json

--- commands/rules.js
+++ commands/rules.js
@@ -6,7 +6,9 @@ const {
   Events,
   AttachmentBuilder,
 } = require("discord.js");
+
 const debug = require("../debug");
+const { rules_embed } = require("../jsons/server-embeds");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,77 +19,6 @@ module.exports = {
       interaction.member.roles.cache.has(process.env.DC_ADMIN_ROLE) ||
       interaction.member.roles.cache.has(process.env.DC_MOD_ROLE)
     ) {
-      const embed = {
-        title: "RULES",
-        description: "*Please read the rules below:*",
-        color: 16034331,
-        image: {
-          url: "https://operationscentre.github.io/community/img/rust-logo.jpg",
-        },
-        fields: [
-          {
-            name: ":one:",
-            value: "You must be at least 16-years-old to play!",
-          },
-          {
-            name: ":two:",
-            value: "No inappropriate names, profile pictures, or status'!",
-          },
-          {
-            name: ":three:",
-            value: "Use the correct channels for what you are posting!",
-          },
-          {
-            name: ":four:",
-            value:
-              "No offensive or inappropriate content, remarks, or song requests!",
-          },
-          {
-            name: ":five:",
-            value:
-              "No NSFW content (NSFW memes are allowed in the meme channel but nothing too obscene)!",
-          },
-          {
-            name: ":six:",
-            value: "No spamming channels!",
-          },
-          {
-            name: ":seven:",
-            value:
-              "No advertising or selling of products, services or other communities!",
-          },
-          {
-            name: ":eight:",
-            value:
-              "No political or religious arguments (Try to keep political and/or religious conversations to a minimum)!",
-          },
-          {
-            name: ":nine:",
-            value: "No loud noises or music played through your microphone!",
-          },
-          {
-            name: ":one::zero:",
-            value:
-              "No abusive talk to admins or moderators (They are here to help)!",
-          },
-          {
-            name: ":one::one:",
-            value:
-              "If you are streaming publicly (Twitch, YouTube, etc.) while in chat you must add a hash symbol (#) to your nickname!",
-          },
-          {
-            name: ":one::two:",
-            value:
-              "Do not use the ticketing system for anything other than a legitimate reason!",
-          },
-          {
-            name: ":one::three:",
-            value:
-              "Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism or hate speech will be tolerated.",
-          },
-        ],
-      };
-
       const row = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
           .setCustomId("rules-button")
@@ -100,7 +31,7 @@ module.exports = {
 
       await interaction.reply({
         files: [attachment1],
-        embeds: [embed],
+        embeds: [rules_embed],
         components: [row],
       });
       interaction.followUp({ files: [attachment2] });
--- jsons/server-embeds.js
+++ jsons/server-embeds.js
@@ -136,4 +136,68 @@ module.exports = {
       },
     ],
   },
+
+  rules_embed: {
+    title: "RULES",
+    description: "*Please read the rules below:*",
+    color: 16034331,
+    image: {
+      url: "https://operationscentre.github.io/community/img/rust-logo.jpg",
+    },
+    fields: [
+      {
+        name: ":one:",
+        value: "You must be at least 16-years-old to play!",
+      },
+      {
+        name: ":two:",
+        value: "No inappropriate names, profile pictures, or status'!",
+      },
+      {
+        name: ":three:",
+        value: "Use the correct channels for what you are posting!",
+      },
+      {
+        name: ":four:",
+        value:
+          "No offensive or inappropriate content, remarks, or song requests!",
+      },
+      {
+        name: ":five:",
+        value: "No NSFW content",
+      },
+      {
+        name: ":six:",
+        value: "No spamming channels!",
+      },
+      {
+        name: ":seven:",
+        value:
+          "No advertising or selling of products, services or other communities!",
+      },
+      {
+        name: ":eight:",
+        value:
+          "No political or religious arguments (Try to keep political and/or religious conversations to a minimum)!",
+      },
+      {
+        name: ":nine:",
+        value: "No loud noises or music played through your microphone!",
+      },
+      {
+        name: ":one::zero:",
+        value: "No abusive talk to staff! (They are here to help)!",
+      },
+      {
+        name: ":one::one:",
+        value:
+          "Do not use the ticketing system for anything other than a legitimate reason!",
+      },
+      {
+        name: ":one::two:",
+        value:
+          "Treat everyone with respect. Absolutely no harassment, witch hunting, sexism, racism or hate speech will be tolerated.",
+      },
+    ],
+  },
 };
